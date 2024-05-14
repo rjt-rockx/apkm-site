b2c53570c1d3151391029b5220a02e52109c9aec
rjt <rjt.rockx@gmail.com>
2024-05-15 01:53:04 +0530

    faq section

--- src/routes/premium/+page.svelte
+++ src/routes/premium/+page.svelte
@@ -15,6 +15,9 @@
 	import MicroSD from '~icons/mdi/micro-sd'
 	import Database from '~icons/mdi/database'
 	import Lock from '~icons/mdi/lock'
+	import ChevronRight from '~icons/mdi/chevron-right'
+	import { Accordion } from 'bits-ui'
+	import { slide } from 'svelte/transition'
 
 	const plans = [
 		{
@@ -120,6 +123,65 @@
 			description:
 				'APKMirror verifies cryptographic signatures against Google Play Store independently, ensuring the safety and integrity of the APKs you download.',
 			wide: true
+		}
+	]
+
+	const questions = [
+		{
+			question: 'What happened to the monthly plan?',
+			answer: `When we first launched APKMirror PREMIUM, we started off with a monthly and annual plan. It quickly became apparent that a lower-priced monthly plan was causing a number of issues:
+Fraudsters are attracted to ways to test stolen credit cards with small $ amounts. Each credit card dispute costs us $15, no matter if we are able to win the dispute or not.
+With low plan amounts, such as $1.99, we immediately lost about 25% of it to Stripe fees, which are 2.9% + 30¢ per successful transaction, plus 2¢ per each screened transaction for Radar for Fraud Teams.
+With these in mind, we hope you can understand our reasoning for replacing the monthly plan with a quarterly plan and adjusting pricing.`
+		},
+		{
+			question: 'Is my payment safe?',
+			answer:
+				'APKMirror PREMIUM is powered by Memberful, a Patreon company. Patreon and Memberful utilize the highest standards of safety and manage payments through Google Pay, Apple Pay, and Stripe. Visa, Mastercard, American Express, Discover, and JCB cards are supported. We never see the full details of your payment, like credit card numbers, so your payment information is safe and secure.'
+		},
+		{
+			question: "Why can't I log in?",
+			answer:
+				'Check your browser’s security settings. Our membership software won’t work if you’ve disabled 3rd-party cookies. Either change your settings or use another browser.'
+		},
+		{
+			question: 'Can I update my email address, payment method, or other personal information?',
+			answer: 'Yes. Log in to your account to make these changes.'
+		},
+		{
+			question: 'Can I upgrade/downgrade my plan or adjust the price?',
+			answer: `Yes! If you sign up with a quarterly plan and decide to switch to yearly or vice versa, you can upgrade or downgrade using the Subscriptions menu item of your account. A pro-rated discount or charge will apply.
+Similarly, if you decide to adjust the contribution amount, you can do so from the same area, as long as the new amount is higher than the plan’s minimum.`
+		},
+		{
+			question: 'How do I reset my password?',
+			answer: `Passwords are not required for your membership account. Instead, visit our sign in form, enter your email address and click Send sign in link. We’ll send you an email with a link that you can click to sign in. The link can only be used once and expires after 24 hours.
+If you prefer to create a password, you can do so after you sign up for a membership. If you ever need to reset your password, click this link, enter your email address, and then click send me reset instructions. To ensure the reset process is secure:
+
+Password reset links automatically expire after 48 hours.
+Password reset links can only be used once. If you click the link, and reset your password, the link becomes invalid.
+If the link becomes invalid, delete the old reset email and start with a fresh password reset.`
+		},
+		{
+			question: 'Can I gift a subscription?',
+			answer: `Yes. It’s easy to gift a subscription. Click one of these links: Quarterly | Annual.
+You will be prompted to enter the gift recipient’s name and email address, the delivery date, a personalized message, and your payment information. The gift recipient will receive an email with a link to activate their gift. If a physical address is required, the gift recipient will have the opportunity to enter it when they activate the gift.
+
+Keep in mind that you’re purchasing a one-time gift for the selected term (e.g. quarter or year). The gift recipient will be responsible for adding a payment method before the next renewal period if they’d like to continue with the subscription.`
+		},
+		{
+			question: 'Does APKMirror Premium apply to APKMirror Installer?',
+			answer:
+				'At the moment, the APKMirror Installer ad-free subscription is separate from APKMirror PREMIUM website membership and is handled by Google Play billing. We may consider combining the two in the future, but because subscriptions are handled via two separate systems (Google Play vs Memberful), it’s not a trivial feature to support.'
+		},
+		{
+			question: 'How do I cancel my subscription?',
+			answer: 'Log in to your account and toggle auto-renew off from the Subscriptions menu item.'
+		},
+		{
+			question: 'I have another question not covered in this FAQ.',
+			answer:
+				'If you have another question concerning APKMirror PREMIUM, feel free to get in touch using the contact form.'
 		}
 	]
 
@@ -234,5 +296,30 @@
 				</div>
 			</div>
 		{/each}
+	</section>
+	<div class="mt-2 flex w-full flex-row items-center justify-center px-4">
+		<h2 class="font-bold uppercase tracking-wider text-gray-500">Frequently Asked Questions</h2>
+	</div>
+	<section class="flex w-full flex-col items-start gap-4">
+		<Accordion.Root multiple class="flex h-fit w-full flex-col items-start gap-4">
+			{#each questions as question, index}
+				<Accordion.Item
+					value={`${index}`}
+					class="flex h-fit w-full flex-col rounded bg-white shadow transition-all">
+					<Accordion.Header class="h-fit w-full">
+						<Accordion.Trigger class="group flex w-full flex-row items-center gap-2 p-4">
+							<ChevronRight
+								class="size-5 text-orange-500 transition-transform group-data-[state=open]:rotate-90" />
+							<span class="font-semibold text-gray-900">{question.question}</span>
+						</Accordion.Trigger>
+					</Accordion.Header>
+					<Accordion.Content
+						transition={(node) => slide(node, { duration: 150 })}
+						class="px-4 pb-4">
+						<p class="text-gray-900 text-opacity-80">{question.answer}</p>
+					</Accordion.Content>
+				</Accordion.Item>
+			{/each}
+		</Accordion.Root>
 	</section>
 </article>
